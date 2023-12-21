4278a795dc352b41a09583827e98ec5a403e67bc
Karim Abdukarimov <k_abdukarimov2002@my.com>
2023-12-21 18:41:50 +0600

    create simple test

---
+++ src/test/java/abdukarimov/SearchTest.java
@@ -0,0 +1,12 @@
+package abdukarimov;
+
+import io.qameta.allure.Allure;
+import org.junit.jupiter.api.Test;
+
+public class SearchTest {
+    @Test
+    void test() {
+        Allure.step("Success test");
+    }
+}
+
